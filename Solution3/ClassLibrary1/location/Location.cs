729e8ac3f4b22347b3f6d766c03cb7faf5c5bdca
name <reiojg@example.com>
2026-01-21 09:20:21 +0700

    project

---
+++ Solution3/ClassLibrary1/location/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClassLibrary1.location
+{
+    public class Location
+    {
+        public string Address;
+        public string Name;
+        public DateTime CreatedAt;
+        public Guid ID;
+        public string Timezone;
+        public DateTime UpdatedAt;
+
+
+    }
+}
+
